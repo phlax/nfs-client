70f26be4c42b22db2d882d169f0bc7dc8b351f70
Ryan Northey <ryan@synca.io>
2018-05-16 11:17:27 +0100

    Make entry script work with multiple nfs mounts

--- docker/nfs-entry.sh
+++ docker/nfs-entry.sh
@@ -2,45 +2,89 @@
 
 set -e
 
-mkdir -p "$MOUNTPOINT"
-
-echo "Starting NFS client ====================================="
+echo "Starting NFS clients ===================================="
 
 
 function term_handler () {
-    echo "Stopping NFS client ====================================="
     echo "Unmounting $MOUNTPOINT ($SERVER:$SHARE)"
-    umount -t "$FSTYPE" "$MOUNTPOINT"
+    umount -t "$FSTYPE" "$MOUNTPOINT$SHARE"
     exit 143;
 }
 
+function stop_nfs_import () {
+    IMPORT=$1
+    local NFS_CONFIG_LINE=${!IMPORT}
+    local NFS_CONFIG
+    read -r -a NFS_CONFIG <<< "$NFS_CONFIG_LINE"
+    local NFS_SERVER=${NFS_CONFIG[1]}
+    local NFS_MOUNT=${NFS_CONFIG[2]}
+    echo "Unmount $MOUNTPOINT/$NFS_SERVER$NFS_MOUNT"
+    umount "$MOUNTPOINT/$NFS_SERVER$NFS_MOUNT"
+    # mount -t "$NFS_TYPE" # | grep -e "^$MOUNTPOINT/$NFS_SERVER$NFS_MOUNT"
+}
 
-function mount_nfs_share () {
-    # check if we can resolve the server address, if not wait until we can
-    RESOLVED=$(getent hosts $SERVER | awk '{ print $1 }')
 
+function stop_imports () {
+    echo "Stopping NFS clients ===================================="
+    local NFS_IMPORTS=$(compgen -A variable | grep -E 'NFS_[0-9]$' | sort)
+    for IMPORT in $NFS_IMPORTS; do
+        stop_nfs_import $IMPORT
+    done
+    exit 143;
+}
+
+function mount_nfs_import () {
+    local NFS_TYPE=$1
+    local NFS_SERVER=$2
+    local NFS_MOUNT=$3
+    local NFS_OPTIONS=$4
+    local RESOLVED=$(getent hosts $NFS_SERVER | awk '{ print $1 }')
     if [ -z "$RESOLVED" ];then
-       echo "Unable to resolve server '$SERVER', waiting..."
-       until [ ! -z "$(getent hosts $SERVER | awk '{ print $1 }')" ]; do
+       echo "Unable to resolve server '$NFS_SERVER', waiting..."
+       until [ ! -z "$(getent hosts $NFS_SERVER | awk '{ print $1 }')" ]; do
            sleep 0.1;
        done
     fi
-    RESOLVED=$(getent hosts $SERVER | awk '{ print $1 }')
-    echo "Mounting $SERVER($RESOLVED):$SHARE to $MOUNTPOINT"
-    rpc.statd & rpcbind -f &
-    mount -t "$FSTYPE" -o "$MOUNT_OPTIONS" "$SERVER:$SHARE" "$MOUNTPOINT"
+    RESOLVED=$(getent hosts $NFS_SERVER | awk '{ print $1 }')
+    if [ -z "$NFS_OPTIONS"  ]; then
+        if [ $NFS_TYPE = 'nfs4' ]; then
+            NFS_OPTIONS='nfsvers=4'
+        fi
+    fi
+    # rpc.statd & rpcbind -f &
+    mkdir -p "$MOUNTPOINT/$NFS_SERVER$NFS_MOUNT"
+
+    echo "Mounting $NFS_SERVER($RESOLVED):$NFS_MOUNT to $MOUNTPOINT/$NFS_SERVER$NFS_MOUNT with $NFS_OPTIONS"
+    mount -t "$NFS_TYPE" -o "$NFS_OPTIONS" "$NFS_SERVER:$NFS_MOUNT" "$MOUNTPOINT/$NFS_SERVER$NFS_MOUNT"
+    mount -t "$NFS_TYPE" | grep -E  "^$NFS_SERVER:([/]*)$NFS_MOUNT"
 }
 
-if [ $SERVER == "" ]; then
-    rpc.statd & rpcbind -f & echo "docker NFS client with rpcbind ENABLED... if you wish to mount the mountpoint in this container USE THE FOLLOWING SYNTAX instead:
-    $ docker run -itd --privileged=true --net=host -v vol:/mnt/nfs-1:shared -e SERVER= X.X.X.X -e SHARE=shared_path d3fk/nfs-client" & more
-else
-    mount_nfs_share;
-fi
 
-trap 'term_handler' SIGTERM
+function init_nfs_import () {
+    IMPORT=$1
+    local NFS_CONFIG_LINE=${!IMPORT}
+    local NFS_CONFIG
+    read -r -a NFS_CONFIG <<< "$NFS_CONFIG_LINE"
+    local NFS_TYPE=${NFS_CONFIG[0]}
+    local NFS_SERVER=${NFS_CONFIG[1]}
+    local NFS_MOUNT=${NFS_CONFIG[2]}
+    local NFS_OPTIONS=${NFS_CONFIG[3]}
+    mount_nfs_import $NFS_TYPE $NFS_SERVER $NFS_MOUNT $NFS_OPTIONS
+}
+
+function init_imports () {
+    local NFS_IMPORTS=$(compgen -A variable | grep -E 'NFS_[0-9]$' | sort)
+    for IMPORT in $NFS_IMPORTS; do
+        init_nfs_import $IMPORT
+    done
+}
+
+trap 'stop_imports' SIGTERM SIGINT
+init_imports
 
 while true
 do
   tail -f /dev/null & wait ${!}
 done
+
+exit 0
